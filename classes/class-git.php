157b34e05e6bc2a10f4d710f7d9b4619708ae22c
Cristian <cristian18josue@gmail.com>
2021-06-01 11:55:31 -0500

    Nuevo archivo

---
+++ classes/class-git.php
@@ -0,0 +1,20 @@
+
+<?php
+
+class Coche {
+    
+    //atributos
+    public $velocidad;
+    public $color;
+    public $modelo;
+    
+    
+    // Contructor
+    
+    public function __construct($velocidad, $color, $modelo) {
+        $this->velocidad = $velocidad;
+        $this->color = $color;
+        $this->modelo = $modelo;
+    }
+  
+}
